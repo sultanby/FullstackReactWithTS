cc28cdbd8b962ffedf452f29737b6c6b8d1d1d9e
sultanby <jemjasott@gmail.com>
2021-09-02 14:08:27 +0200

    hook testing

--- goblin-store-testing/src/CartContext/CartContext.tsx
+++ goblin-store-testing/src/CartContext/CartContext.tsx
@@ -1,68 +1,20 @@
 import React from "react"
-import { Product } from "../shared/types"
+import {useCart} from './useCart'
 
-interface CartContextValue {
-  addToCart: (product: Product) => void
-  totalPrice: () => number
-  removeFromCart: (product: Product) => void
-  clearCart: () => void
-  products: Product[]
+type CartContextValue = ReturnType<typeof useCart>
+
+export const CartContext = React.createContext({} as CartContextValue)
+
+interface CartProviderProps {
+  useCartHook?: typeof useCart;
 }
 
-const saveProducts = (products: Product[]) => {
-  localStorage.setItem("products", JSON.stringify(products))
-}
-
-const CartContext = React.createContext({} as CartContextValue)
-
-export const CartProvider = ({ children }: React.PropsWithChildren<{}>) => {
-  const [products, setProducts] = React.useState<Product[]>([])
-
-  React.useEffect(() => {
-    try {
-      const storedProducts = localStorage.getItem("products")
-      const parsedProducts = storedProducts ? JSON.parse(storedProducts) : []
-      setProducts(parsedProducts)
-    } catch (error) {}
-  }, [])
-
-  const totalPrice = () => {
-    return products.reduce((total: number, product) => {
-      return total + Number(product.price)
-    }, 0)
-  }
-
-  const addToCart = (newProduct: Product) => {
-    if (products.find((product) => newProduct.name === product.name)) {
-      return
-    }
-    const newProducts = [...products, newProduct]
-    setProducts(newProducts)
-    saveProducts(newProducts)
-  }
-
-  const removeFromCart = (product: Product) => {
-    const newProducts = products.filter((p) => {
-      return p.name !== product.name
-    })
-    setProducts(newProducts)
-    saveProducts(newProducts)
-  }
-
-  const clearCart = () => {
-    setProducts([])
-    saveProducts([])
-  }
-
+export const CartProvider = ({ children, useCartHook = useCart }: React.PropsWithChildren<CartProviderProps>) => {
+  const value = useCartHook()
+ 
   return (
     <CartContext.Provider
-      value={{
-        addToCart,
-        removeFromCart,
-        clearCart,
-        products,
-        totalPrice,
-      }}
+      value={value}
     >
       {children}
     </CartContext.Provider>
