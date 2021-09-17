4232a2d222dcd0eb0a05d453768f25228defc773
sultanby <jemjasott@gmail.com>
2021-09-17 15:05:47 +0200

    instrument list

--- react-piano/src/components/Keyboard/WithInstrument.tsx
+++ react-piano/src/components/Keyboard/WithInstrument.tsx
@@ -1,13 +1,19 @@
-import React, { FunctionComponent } from "react"
-import { useAudioContext } from "../AudioContextProvider/useAudioContext" 
-import { useSoundfont } from "../../adapters/Soundfont/useSoundfont" 
+import React, { FunctionComponent, useEffect } from "react"
+import { useAudioContext } from "../AudioContextProvider/useAudioContext"
+import { useSoundfont } from "../../adapters/Soundfont/useSoundfont"
 import { useMount } from "../../utils/useMount/useMount"
 import { Keyboard } from "../Keyboard/Keyboard"
 import "./style.css"
+import { useInstrument } from "../../state/Instrument/Context"
 
 export const KeyboardWithInstrument: FunctionComponent = () => {
     const AudioContext = useAudioContext()!
-    const { loading, play, stop, load } = useSoundfont({ AudioContext })
-    useMount(load)
+    const { instrument } = useInstrument()
+    const { loading, current, play, stop, load } = useSoundfont({
+        AudioContext
+    })
+    useEffect(() => {
+        if (!loading && instrument !== current) load(instrument)
+    }, [load, loading, current, instrument])
     return <Keyboard loading={loading} play={play} stop={stop} />
 }
--- react-piano/src/components/Main/Main.tsx
+++ react-piano/src/components/Main/Main.tsx
@@ -1,12 +1,12 @@
 import React, { FunctionComponent } from "react"
-import { KeyboardWithInstrument } from "../Keyboard/WithInstrument" 
 import { NoAudioMessage } from "../NoAudioMessage/NoAudioMessage" 
 import { useAudioContext } from "../AudioContextProvider/useAudioContext"
+import { Playground } from "../Playground/Playground"
 
 export const Main: FunctionComponent = () => {
     const AudioContext = useAudioContext() 
     return !!AudioContext ? (
-        <KeyboardWithInstrument />) : (
+        <Playground />) : (
             <NoAudioMessage />
         )
 }
