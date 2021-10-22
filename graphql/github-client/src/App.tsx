300bba522e4e1031333aa45c1d93212b53bc9d71
sultanby <jemjasott@gmail.com>
2021-10-22 22:55:39 +0200

    init

---
+++ graphql/github-client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export const App = () => {
+    return (
+        <blessed-box style={{
+            bg: "#0000ff"
+        }}
+        >
+            Hello React-Blessed
+        </blessed-box>
+    )
+}
